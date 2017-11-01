136c15de3e149e74df30010cd77289167dcbd7e1
mornya <mornya@naver.com>
2017-11-01 16:58:37 +0900

    [1.4.x] Dependency update / README 수정 / indent 수정

--- packages/react-scripts/template/src/reducers.js
+++ packages/react-scripts/template/src/reducers.js
@@ -2,6 +2,6 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
-    /* Insert reducers here */
-    routing: routerReducer
+  /* Insert reducers here */
+  routing: routerReducer,
 });
